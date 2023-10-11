8a609e96e2c1d93bc4b1e2faa9b3ccf8e91f277b
rashmin <rashminkachhadiya97273@gmail.com>
2023-10-11 11:32:46 +0100

    Changes

--- resources/views/backend/admin/order/pdf_supplier.blade.php
+++ resources/views/backend/admin/order/pdf_supplier.blade.php
@@ -28,7 +28,8 @@
         }
         #details {
           /*border: 1px solid black;*/
-          border-collapse: collapse;
+          border-spacing: 0px;
+           page-break-inside: avoid;
         }
         .td_tag {
             font-size: 12px; 
@@ -73,31 +74,34 @@
                             <tr>
                                 <td width="30%" align="left" valign="top">
                                      @foreach($order->orderPicture as $image)
-                                        <img width="170px;" height="170px" src="{{asset('assets/images/users/order/').'/'.$image->images}}">
+                                        <img width="140px;" height="140px" src="{{asset('assets/images/users/order/').'/'.$image->images}}">
                                     @endforeach
                                 </td>
                                 <td width="70%" align="center" valign="top">
                                     <table id="details" width="100%" border="0" cellspacing="1" cellpadding="0">
                                         <tbody>
                                             <tr style="">
-                                                <td width="10%" height="25" align="center" bgcolor="#e5dddf" valign="middle" style="border-right: 1px solid #fff"><span style="font-size: 12px; color: #78bd7d;">Date</span></td>
-                                                <td width="10%" height="25" align="center" bgcolor="#e5dddf" valign="middle" style="border-right: 1px solid #fff"><span style="font-size: 12px; color: #78bd7d;">Order</span></td>
-                                                <td width="10%" height="25" align="center" bgcolor="#e5dddf" valign="middle" style="border-right: 1px solid #fff"><span style="font-size: 12px; color: #78bd7d;">Name</span></td>
-                                                <td width="10%" height="25" align="center" bgcolor="#e5dddf" valign="middle" style="border-right: 1px solid #fff"><span style="font-size: 12px; color: #78bd7d;">Code</span></td>
-                                                <td width="10%" height="25" align="center" bgcolor="#e5dddf" valign="middle" style="border-right: 1px solid #fff"><span style="font-size: 12px; color: #78bd7d;">Size</span></td>
-                                                <td width="10%" height="25" align="center" bgcolor="#e5dddf" valign="middle" style="border-right: 1px solid #fff"><span style="font-size: 12px; color: #78bd7d;">Qty</span></td>
-                                                <td width="10%" height="25" align="center" bgcolor="#e5dddf" valign="middle" style="border-right: 1px solid #fff"><span style="font-size: 12px; color: #78bd7d;">Metal</span></td>
+                                                <td width="10%" height="25" align="center" bgcolor="#000" valign="middle" style="border-right: 1px solid #000"><span style="font-size: 12px; color: #fff;">Date</span></td>
+                                                <td width="10%" height="25" align="center" bgcolor="#000" valign="middle" style="border-right: 1px solid #000"><span style="font-size: 12px; color: #fff;">Order</span></td>
+                                                <td width="10%" height="25" align="center" bgcolor="#000" valign="middle" style="border-right: 1px solid #000"><span style="font-size: 12px; color: #fff;">Name</span></td>
+                                                <td width="10%" height="25" align="center" bgcolor="#000" valign="middle" style="border-right: 1px solid #000"><span style="font-size: 12px; color: #fff;">Code</span></td>
+                                                <td width="10%" height="25" align="center" bgcolor="#000" valign="middle" style="border-right: 1px solid #000"><span style="font-size: 12px; color: #fff;">Size</span></td>
+                                                <td width="10%" height="25" align="center" bgcolor="#000" valign="middle" style="border-right: 1px solid #000"><span style="font-size: 12px; color: #fff;">Qty</span></td>
+                                                <td width="10%" height="25" align="center" bgcolor="#000" valign="middle" style="border-right: 1px solid #000"><span style="font-size: 12px; color: #fff;">Metal</span></td>
                                             </tr>
                                             <tr>
-                                                <td class="td_tag" align="center">{{ \Carbon\Carbon::parse($order->created_at)->format('d/m/Y') }}</td>
-                                                <td class="td_tag" align="center">{{ $order->order_number }}</td>
-                                                <td class="td_tag" align="center">{{ $supplier[$order->supplier_name] }}</td>
-                                                <td class="td_tag" align="center">{{ $order->sku }}</td>
-                                                <td class="td_tag" align="center">{{ $order->size }}</td>
-                                                <td class="td_tag" align="center">{{ $order->quantity }}</td>
-                                                <td class="td_tag" align="center">@if(!is_null($order->metal_type))
+                                                <td class="" style="font-size: 12px; line-height: 20px;border:1px solid black;border-collapse: collapse;border-spacing: -1px;" align="center">{{ \Carbon\Carbon::parse($order->created_at)->format('d/m/Y') }}</td>
+                                                <td class="" style="font-size: 12px; line-height: 20px;border:1px solid black;border-collapse: collapse;border-spacing: -1px;" align="center">{{ $order->order_number }}</td>
+                                                <td class="" style="font-size: 12px; line-height: 20px;border:1px solid black;border-collapse: collapse;border-spacing: -1px;" align="center">{{ $supplier[$order->supplier_name] }}</td>
+                                                <td class="" style="font-size: 12px; line-height: 20px;border:1px solid black;border-collapse: collapse;border-spacing: -1px;" align="center">{{ $order->sku }}</td>
+                                                <td class="" style="font-size: 12px; line-height: 20px;border:1px solid black;border-collapse: collapse;border-spacing: -1px;" align="center">{{ $order->size }}</td>
+                                                <td class="" style="font-size: 12px; line-height: 20px;border:1px solid black;border-collapse: collapse;border-spacing: -1px;" align="center">{{ $order->quantity }}</td>
+                                                <td class="" style="font-size: 12px; line-height: 20px;border:1px solid black;border-collapse: collapse;border-spacing: -1px;" align="center">@if(!is_null($order->metal_type))
                                                     {{ config('params.metal_type')[$order->metal_type] }}
                                                     @endif</td>
+                                            </tr>
+                                            <tr>
+                                                <td>&nbsp;</td>
                                             </tr>
                                             <tr>
                                                 <td>Note: </td>
