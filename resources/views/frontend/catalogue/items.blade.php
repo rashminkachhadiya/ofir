7a8345e5753d5fcdd6a94f91456942b6513bc7e4
rashmin <rashminkachhadiya97273@gmail.com>
2023-10-16 10:18:47 +0100

    Changes

--- resources/views/frontend/catalogue/items.blade.php
+++ resources/views/frontend/catalogue/items.blade.php
@@ -95,7 +95,7 @@ div.content {
 </style>
     <div class="back">
         <div class="div-center">
-            <div class="mb-4 d-flex">
+            <div class="mb-4 d-flex" style="border-bottom: 1px solid black;">
               <div class="col-md-3">
                 <a class="m-2 mt-0" style="font-size: 25px;cursor: pointer;" href="{{ URL::to('/catalogue/'.$mainCatalogue) }}"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z"/></svg></a>
 
@@ -108,7 +108,7 @@ div.content {
                 <h6 style="color:black;font-style: italic; text-align: right;"><a style="color: black;" href="{{ URL::to('/cart') }}">cart</a></h6>
               </div>
             </div>
-            <div class="row">
+            <div class="row" style="height: 500px; overflow-y: auto;">
                 @forelse($items as $item)
                   <div class="col-md-4 text-center">
                     <div>
@@ -144,7 +144,7 @@ div.content {
                   </div>
                 @endforelse
             </div>
-            <div class="paginatoin-area text-center mt-0 mb-0 d-flex">
+            <div class="paginatoin-area text-center mt-0 mb-0 d-flex" style="border-top:1px solid black;">
                 @if($page != 'all_product')
                 {{ $items->appends(request()->input())->links('vendor.pagination.default') }}
                 @else
