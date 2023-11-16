952e1f3d1503ce1a4d678518356ac1044a5fc356
rashmin <rashminkachhadiya97273@gmail.com>
2023-11-16 15:09:36 +0000

    Changes

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -143,7 +143,7 @@ class OrderExport implements FromCollection, WithHeadings, WithColumnWidths,With
                 //Set row height
                 // for ($i = 2; $i < count($orders); $i++) //iterate based on row count
                 // {
-                    $event->sheet->getDelegate()->setSize('B1', 100, 100);
+                    $event->sheet->getRowDimension(2)->setRowHeight(100);
                 // }
    
             },
