87fb97ca8e161ce8f34aeb99de22a3eec3318b44
rashmin <rashminkachhadiya97273@gmail.com>
2024-06-07 10:19:35 +0100

    'change'

--- app/Http/Controllers/Backend/Admin/StockController.php
+++ app/Http/Controllers/Backend/Admin/StockController.php
@@ -19,9 +19,9 @@ class StockController extends Controller
      */
     public function index()
     {
-        // $item = ItemStock::where('item_status',1)->delete();
-        // echo "delete";
-        // die;
+        $item = ItemStock::where('item_status',0)->delete();
+        echo "delete appro";
+        die;
         $itemStatus = ['0'=>'Apro','1'=>'Sold','2' => 'In Stock'];
         $catalogues = config('params.catalogue');
         $catalogues[''] = 'All Catalogue';
