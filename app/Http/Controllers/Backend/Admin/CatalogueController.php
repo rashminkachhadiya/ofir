ba5bfa656afe4dc8282ded994f131e44bf8df2b3
rashmin <rashminkachhadiya97273@gmail.com>
2024-07-03 13:19:48 +0100

    'changes'

--- app/Http/Controllers/Backend/Admin/CatalogueController.php
+++ app/Http/Controllers/Backend/Admin/CatalogueController.php
@@ -37,7 +37,7 @@ class CatalogueController extends Controller
       //   print_r($allCatalogue);
       //   die;
 
-      for ($i=4008; $i < 4018 ; $i++) {
+      for ($i=4020; $i < 4032 ; $i++) {
           $item = new Item();
           $item->sku = 'LE'.$i;
           $item->catalogue_id = 11;
