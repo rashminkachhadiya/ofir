3cfa3cda769296bad104be88c5302467fc936271
rashmin <rashminkachhadiya97273@gmail.com>
2024-07-08 16:32:14 +0100

    'changes'

--- app/Http/Controllers/Backend/Admin/CatalogueController.php
+++ app/Http/Controllers/Backend/Admin/CatalogueController.php
@@ -59,7 +59,7 @@ class CatalogueController extends Controller
       // }
         $catalogues = config('params.catalogue');
         $catalogues[''] = 'All Catalogue';
-        $subCatalogue = config('params.1');
+        $subCatalogue = config('params.11');
         $subCatalogue[''] = 'All Sub Catalogue';
         $inStock = config('params.in_stock');
         $inStock[''] = 'All Stock';
